5f72e6e72acc60b518e5bcb97222f65af19c3d22
Chenghan Xie <1752624073@qq.com>
2023-10-14 21:24:11 -0700

    atari_fig

--- results/plot.py
+++ results/plot.py
@@ -28,13 +28,17 @@ import numpy as np
 # plt.show()
 
 # epsoids = 15
-path1='./run-dmc_walker_walk_v2_2-tag-scalars_train_return.csv'
-path2='./run-dmc_walker_walk_v3-tag-scalars_train_return.csv'
+path1='./run-atari_pong_v3-tag-scalars_train_return.csv'
+path2='./run-atari_pong_v2_6S-tag-scalars_train_return.csv'
+path3='./run-atari_pong_v2_5-tag-scalars_train_return (1).csv'
+path4='./run-atari_pong_v2_6S-tag-scalars_train_return.csv'
 
 
 
-v2 = pd.read_csv(path1)
-v3=pd.read_csv(path2)
+v3 = pd.read_csv(path1)
+v2S=pd.read_csv(path2)
+v2M=pd.read_csv(path3)
+v2L=pd.read_csv(path4)
 # drsom2=pd.read_csv(path6)
 
 
@@ -42,7 +46,9 @@ plt.figure(dpi=300,figsize=(8,4))
 plt.title('')
 # plt.plot(np.arange(1, epsoids + 1), sgd1["all_val_mae"], label='sgd,lr=0.001')
 # plt.plot(np.arange(1, epsoids + 1), nsgd2["all_val_mae"], label='nsgd,lr=0.001')
-plt.plot(v2["Step"][:50], v2["Value"][0:50], label='DreamerV2')
+plt.plot(v2S["Step"][:50], v2S["Value"][0:50], label='DreamerV2_S')
+plt.plot(v2M["Step"][:50], v2M["Value"][0:50], label='DreamerV2_M')
+plt.plot(v2L["Step"][:50], v2L["Value"][0:50], label='DreamerV2_L')
 plt.plot(v3["Step"][:50], v3["Value"][0:50], label='DreamerV3')
 # plt.plot(np.arange(1, epsoids + 1), drsom1["all_val_mae"], label='drsom,new para')
 # plt.plot(np.arange(1, epsoids + 1), drsom2["all_val_mae"], label='drsom,gamma=1e-3')
@@ -52,5 +58,5 @@ plt.plot(v3["Step"][:50], v3["Value"][0:50], label='DreamerV3')
 plt.legend()
 plt.ylabel('Return')
 plt.xlabel('Env Steps')
-plt.savefig('./fig/dmc_walk_walker.jpg')
+plt.savefig('./fig/atari_pong.jpg')
 plt.show()
